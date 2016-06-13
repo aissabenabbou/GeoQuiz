ec8ffd57d927ab6732b98e0d2ae621de45bf5ff5
aissabenabbou <aissabenabbou@gmail.com>
2016-06-13 14:03:58 +0100

    Add Questions to View

--- app/src/main/java/com/example/engeneer0x0/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/engeneer0x0/geoquiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,24 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonTrue ;
     private Button buttonFalse ;
 
+    private Question [] questions = new Question[]{
+            new Question(R.string.adrar_question, false),
+            new Question(R.string.oran_question, false),
+            new Question(R.string.algies_question, false)
+
+    };
+
+    private int currentIndex = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView questionTextView = (TextView) findViewById(R.id.question_view);
+        int question = questions[currentIndex].getMtextResId();
+        questionTextView.setText(question);
+
 
         buttonFalse = (Button) findViewById(R.id.false_button) ;
         buttonTrue = (Button) findViewById(R.id.true_button) ;
@@ -35,5 +49,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,R.string.Toast_correct,Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
     }
 }
