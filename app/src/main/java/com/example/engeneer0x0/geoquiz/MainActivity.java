3a3078f87bb0e0f1cda958bec06ab3197fede908
benabbou <aissabenabbou>
2016-06-14 07:17:27 +0100

    fix rotation problem

--- app/src/main/java/com/example/engeneer0x0/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/engeneer0x0/geoquiz/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.engeneer0x0.geoquiz;
 
+import android.nfc.Tag;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity" ;
+    private static final String KEY_INDEX = "INDEX" ;
     private Button buttonTrue ;
     private Button buttonFalse ;
     private Button buttonNext ;
@@ -45,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"Oncreate Called");
         setContentView(R.layout.activity_main);
 
          questionTextView = (TextView) findViewById(R.id.question_view);
@@ -79,8 +84,46 @@ public class MainActivity extends AppCompatActivity {
                updateQuestion();
             }
         });
-
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
         updateQuestion();
 
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"On Save Instance State ");
+        savedInstanceState.putInt(KEY_INDEX,currentIndex);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause called");
+
+    }
+    @Override
+    public void  onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume called");
+
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop called");
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy called");
+    }
+
 }
